5df666b9f05fcb29a63815775ddb7163f70e6d3d
Deth Boi <derangedxpuppies@gmail.com>
2026-02-11 20:35:52 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// ... (paste the rooms object, renderRoom, changeRoom, examine from before)
+
+function examine(item) {
+  alert("Whoa dude... this " + item + " is totally tubular.\n\n(press OK to continue)");
+  // Later: replace alert with a <div> modal styled like a Windows 95 dialog
+}
